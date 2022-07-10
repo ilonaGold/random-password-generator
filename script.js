c52ff51b852904ed2bb60d3a9e0e7e00c00d51e5
Ilona Goldmane <songa17@inbox.lv>
2022-07-10 18:02:58 +0300

    Update base javascript

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-             "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-             "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-             "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "!", "@",
-             "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[",
-             "]", "{", "}", "|", ";", ":", ",", ".", "/", "?"  
-             ]
+const chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+               "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+               "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+               "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+               "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!",
+               "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=",
+               "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"
+               ];
 
 const pwdLength = document.getElementById("pwd-length");
-const btn = document.getElementById("btn");
-let password = document.querySelectorAll('.password');
+const btnGenerate = document.getElementById("btn-generate");
+let password = document.querySelectorAll(".password");
 
-btn.addEventListener("click", renderPassword);
+btnGenerate.addEventListener("click", renderPassword);
 
 function generatePassword() {
     password.value = "";
@@ -30,17 +30,17 @@ function renderPassword() {
 
 /* light theme */
 
-const toggle = document.getElementById('toggle');
+const toggle = document.getElementById("toggle");
 
-const container = document.querySelector('.container');
-const accent = document.querySelector('.text-accent');
-const green = document.querySelector('.text-green');
-const subtitle = document.querySelector('.subtitle');
-const lengthSave = document.querySelector('.flex-right');
-const generator = document.querySelector('.generator-module');
+const container = document.querySelector(".container");
+const accent = document.querySelector(".text-accent");
+const green = document.querySelector(".text-green");
+const subtitle = document.querySelector(".subtitle");
+const filter = document.querySelector(".password-filter");
+const generator = document.querySelector(".generator-module");
 
 
-toggle.addEventListener('input', e => {
+toggle.addEventListener("input", e => {
     const isChecked = e.target.checked;
     
     if(isChecked) {
@@ -48,14 +48,14 @@ toggle.addEventListener('input', e => {
         accent.classList.add('light-theme');
         green.classList.add('light-theme');
         subtitle.classList.add('light-theme');
-        lengthSave.classList.add('light-theme');
+        filter.classList.add('light-theme');
         generator.classList.add('light-theme');
     } else {
         container.classList.remove('light-theme');
         accent.classList.remove('light-theme');
         green.classList.remove('light-theme');
         subtitle.classList.remove('light-theme');
-        lengthSave.classList.remove('light-theme');
+        filter.classList.remove('light-theme');
         generator.classList.remove('light-theme');
     }
 });
@@ -66,13 +66,13 @@ toggle.addEventListener('input', e => {
 const rangeInputs = document.querySelectorAll('input[type="range"]');
 
 rangeInputs.forEach(input => {
-  input.addEventListener('input', handleInputChange);
+  input.addEventListener("input", handleInputChange);
 })
 
 function handleInputChange(e) {
   let target = e.target;
-  if(e.target.type !== 'range') {
-    target = document.getElementById('pwd-length');
+  if(e.target.type !== "range") {
+    target = document.getElementById("pwd-length");
   } 
   const min = target.min;
   const max = target.max;
